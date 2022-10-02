5d916f4b94afd20eb987d678a13205faec52b039
Faysal Regal <faysalregal@gmail.com>
2022-10-02 18:18:07 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_Intermediate_Demo_Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Csharp-Intermediate-Demo-Fields
+ 
